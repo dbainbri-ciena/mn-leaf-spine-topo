d2c73ec99ab28c2887b778dc3ff36d1065daf48c
David K. Bainbridge <dbainbri@ciena.com>
2018-08-14 14:39:45 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # mn-leaf-spine-topo
 A mininet based leaf-spine topology
+
+## Quick Start
+Included in this repo is a `Vagrantfile` that can be used to create a VM on
+which the leaf-spine topology can be run. To create the VM a simple `vagrant up`
+should do.
+```
+vagrant up
+vagrant ssh
+cd /vagrant
+```
+
+### Start the SDN Controller
+The script which wraps the leaf-spine topology only supports ONOS currently.
+To start and instance of the ONOS controller as container the following 
+command should work.
+```
+docker run -tid --name onos --rm \
+	-p 8101:8101 -p 8181:8181 -p 6653:6653 \
+	-e ONOS_APPS=openflow,segmentrouting,layout \
+	onosproject/onos:1.13.2
+```
+
+### Create the Topology
+To create a simple 3 leaf / 2 spine topology the following command can be used.
+```
+sudo ./lsnet.py \
+	--controller=remote,ip=127.0.0.1,port=6653 \
+	--leaves=3 \
+	--spines=2 \
+	--hosts=2 \
+	--wait \
+	--ping \
+	--onos=http://karaf:karaf@localhost:8181 \
+	--post
+```
+
+This command will create the topology, wait for the switches to be connected
+to the controller, have each host ping its leaf switch (for arps), and
+generate and `POST` the segment routine configuration to ONOS.
+
+_NOTE: if you add a `-v` command line argument to the `lsnet.py` command to see
+more debug output from the script as the topology is created_
+
+**NOTE: I don't know why this is, but if you execute a `pingall` at the mininet
+prompt it really doesn't work. But if you `ctrl-D` out of mininet and restart
+it with the same parameters and then do a `pingall` it seems to work. I think
+this has something to do with a required order in ONOS between the config push
+and the devices being connected and I consider it a big in ONOS or the SR 
+application.**
+
+### PingAll
+At the mininet prompt execute the `pingall` command to have every host ping
+across the fabric. The output should be something like the following.
+```
+mininet> pingall
+*** Ping: testing ping reachability
+h1 -> h2 h3 h4 h5 h6
+h2 -> h1 h3 h4 h5 h6
+h3 -> h1 h2 h4 h5 h6
+h4 -> h1 h2 h3 h5 h6
+h5 -> h1 h2 h3 h4 h6
+h6 -> h1 h2 h3 h4 h5
+*** Results: 0% dropped (30/30 received)
+```
+
+### View the Topology
+Open your browser and view `http://localhost:8181/onos/ui` and you
+should see the topology. If you want to see the topology in an access layout
+issue the following commands via the ONOS CLI.
+```
+ssh -p 8101 karaf@localhost topo-layout access
+```
+To revert to the default layout use
+```
+ssh -p 8101 karaf@localhost topo-layout default
+```
+_NOTE: the default password is `karaf`_
---
+++ lsnet.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+import argparse
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.node import UserSwitch, RemoteController, DefaultController, NullController, OVSController, Controller
+from mininet.topo import Topo
+from mininet.log import lg, error, debug, info
+from mininet.util import irange, quietRun
+from mininet.link import TCLink
+from functools import partial
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+import sys
+import requests
+
+flush = sys.stdout.flush
+
+class LeafSpineTopo(Topo):
+    "Topology for a leaf spine network"
+
+    def __init__(self, nSpines, nLeaves, nHosts, **params):
+        Topo.__init__(self, **params)
+
+        # Add switches
+        spines={}
+        leaves={}
+        h={}
+        sc=0
+        hc=1
+        for i in range(0, nSpines):
+            spines[i] = self.addSwitch('s%d' % (sc+1))
+            sc += 1
+        for i in range(0, nLeaves):
+            leaves[i] = self.addSwitch('s%d' % (sc+1))
+            for s in spines:
+                self.addLink(leaves[i], spines[s])
+            for j in range(0, nHosts):
+                name = 'h%d' % hc
+                ip = '10.0.%d.%d/24'%(i+1,j+2)
+                hc += 1
+                h[name] = self.addHost(name, ip=ip)
+                debug('*** Add host %s as %s\n' % (name, ip))
+                self.addLink(h[name], leaves[i])
+            sc += 1
+
+def leafSpineNetwork(nSpines, nLeaves, nHosts, controller, ping, generate, wait, onos, post):
+    "Create the network"
+
+    topo = LeafSpineTopo(nSpines, nLeaves, nHosts)
+
+    # Evaluate controller argument
+    terms=controller.split(',')
+    ctype = terms[0]
+    del terms[0]
+    if ctype == 'none':
+        c = NullController('c0')
+    elif ctype == 'remote':
+        ip='127.0.0.1'
+        port=6653
+        for t in terms:
+            kv = t.split('=')
+            if kv[0] == 'ip':
+                ip = kv[1]
+            elif kv[0] == 'port':
+                port=int(kv[1])
+        c = RemoteController('c0', ip=ip, port=port)
+    else:
+        error('*** Unknown controller type\n')
+        return
+     
+    net = Mininet(topo=topo, controller=c, waitConnected=wait)
+
+    net.start()
+    for h in net.hosts:
+        debug('*** %s : disable ipv6\n' % h)
+        h.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        h.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        h.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    for sw in net.switches:
+        debug('*** %s : disable ipv6\n' %sw)
+        sw.cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+        sw.cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+        sw.cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    for h in net.hosts:
+        o = h.IP().split('.')
+        h.cmd('ip route add default via %s.%s.%s.254' % (o[0], o[1], o[2]))
+        if ping:
+            h.cmd('ping -c 1 -t 1 %s.%s.%s.254' % (o[0], o[1], o[2]))
+
+    if generate or post:
+        output = StringIO()
+        print >>output, '{'
+        print >>output, '    "devices": {'
+        for i in range(0, nSpines):
+            print >>output, '        "of:%016x": {' % (i+1)
+            print >>output, '            "segmentrouting": {'
+            print >>output, '                "name": "Spine%d",' % (i+1)
+            print >>output, '                "ipv4NodeSid": %d,' % (101 + i)
+            print >>output, '                "ipv4Loopback": "192.168.0.%d",' % (201 + i)
+            print >>output, '                "routerMac": "c0:ff:ee:00:00:%02x",' % (i+1)
+            print >>output, '                "isEdgeRouter": false,'
+            print >>output, '                "adjacencySids": []'
+            print >>output, '            },'
+            print >>output, '            "basic": {'
+            print >>output, '                "name": "Spine%d",' % (i+1)
+            print >>output, '                "driver": "ofdpa-ovs"'
+            print >>output, '            }'
+            print >>output, '        },'
+        for i in range(0, nLeaves):
+            print >>output, '        "of:%016x": {' % (nSpines+i+1)
+            print >>output, '            "segmentrouting": {'
+            print >>output, '                "name": "Leaf%d",' % (i+1)
+            print >>output, '                "ipv4NodeSid": %d,' % (101 + nSpines+i)
+            print >>output, '                "ipv4Loopback": "192.168.0.%d",' % (201 + nSpines+i)
+            print >>output, '                "routerMac": "c0:ff:ee:00:00:%02x",' % (nSpines+i+1)
+            print >>output, '                "isEdgeRouter": true,'
+            print >>output, '                "adjacencySids": []'
+            print >>output, '            },'
+            print >>output, '            "basic": {'
+            print >>output, '                "name": "Leaf%d",' % (i+1)
+            print >>output, '                "driver": "ofdpa-ovs"'
+            print >>output, '            }'
+            print >>output, '        }%s' % (',' if i+1 < nLeaves else '')
+        print >>output, '    },'
+        print >>output, '    "ports": {'
+        cnt = 0
+        for i in range(0, nLeaves):
+            for j in range(0, nHosts):
+                print >>output, '        "of:%016x/%d": {' % (nSpines+i+1, nSpines+j+1)
+                print >>output, '            "interfaces": [ {'
+                print >>output, '                "ips": [ "10.0.%d.254/24" ],' % (i+1)
+                print >>output, '                "vlan-untagged": 10'
+                print >>output, '            } ]'
+                print >>output, '        }%s' % (',' if cnt+1 < nLeaves*nHosts else '')
+                cnt += 1
+        print >>output, '    }'
+        print >>output, '}'
+        if generate:
+            print output.getvalue()
+        if post:
+            import json
+            info('*** POSTing ONOS network configuration to %s/onos/v1/network/configuration\n' % onos)
+
+            #req.add_header('Content-Type', 'application/json')
+            response = requests.post('%s/onos/v1/network/configuration' % onos,
+                headers={'Content-type': 'application/json'},
+                data=output.getvalue())
+            #response = urllib2.urlopen(req, data=output.getvalue())
+            if int(response.status_code / 100) != 2:
+                error('*** failed to post configuration to ONOS: %d:%s\n' % (ONOSresponse.status_code, response.text))
+            response.close()
+        output.close()
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--leaves', '-l', metavar='N', default=3, type=int, nargs='?',
+        help='the number of leaf nodes')
+    parser.add_argument('--spines', '-s', metavar='N', default=2, type=int, nargs='?',
+        help='the number of spine nodes')
+    parser.add_argument('--hosts', '-t', metavar='N', default=2, type=int, nargs='?',
+        help='the number of hosts per leaf node')
+    parser.add_argument('--controller', '-c', metavar='CONTROLLER', default='remote,ip=127.0.0.1,port=6653', type=str, nargs='?',
+        help='the number of hosts per leaf node')
+    parser.add_argument('--ping', '-p', action='store_true',
+        help='have each host ping the leaf switch')
+    parser.add_argument('--verbose', '-v', action='store_true',
+        help='display a bit more detailed logging')
+    parser.add_argument('--generate', '-g', action='store_true',
+        help='gerate ONOS network configuration for fabric')
+    parser.add_argument('--wait', '-w', action='store_true',
+        help='wait for switches to connect to controller')
+    parser.add_argument('--onos', '-o', metavar='ONOS', default='http://karaf:karaf@127.0.0.1:8181', type=str, nargs='?',
+        help='ONOS base URL')
+    parser.add_argument('--post', '-d', action='store_true',
+        help='post configuration to onos')
+
+    args = parser.parse_args()
+
+    lg.setLogLevel('debug' if args.verbose else 'info')
+    info('*** Starting Leaf - Spine Switches ***\n')
+    leafSpineNetwork(args.spines, args.leaves, args.hosts, args.controller, args.ping, args.generate, args.wait, args.onos, args.post)
